657b609206fe9c9ca6c253c1ccf7a17c12a32a79
mrityunjaykumar31 <mrityunjaykumar31@gmail.com>
2021-03-03 17:29:24 +0530

    jest configured

---
+++ src/controller/UserController.spec.ts
@@ -0,0 +1,17 @@
+  import {UserController}  from './UserController';
+  import {getRepository} from "typeorm";
+ 
+ // const UserController  = require('./UserController');
+// const getRepository = require('typeorm');
+// //import {getRepository} from "typeorm";
+
+// //const uc = UserController;
+describe('Post Endpoints', () => { 
+
+    
+    it("GET /states - success", async () => {
+
+      //  const { body } = await request(app).get("/states"); //uses the request function that calls on express app instance
+ 
+      });
+})
